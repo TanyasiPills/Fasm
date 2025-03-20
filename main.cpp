77ddd9e3070b6a38b0c874a645c14d60a50688b7
Maychii <106334255+TanyasiPills@users.noreply.github.com>
2025-03-20 08:41:46 +0100

    functional input taker :>

--- main.cpp
+++ main.cpp
@@ -1,4 +1,43 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(){
+extern "C" {
+    void nya(void);
 
+    char* getTitle(void);
+
+    int readLine(char* dest, int maxLen);
+}
+
+int readLine(char* dest, int maxLen){
+    char* result = fgets(dest, maxLen, stdin);
+    if(result != NULL)
+    {
+        int len = strlen(result);
+        if(len > 0)
+        {
+            dest[len-1] = 0;
+        }
+        return len;
+    }
+    return -1;
+}
+
+int main(void)
+{
+    try
+    {
+        char* title = getTitle();
+
+        printf("Calling %s:\n", title);
+        nya();
+        printf("%s terminated\n", title);
+    }
+    catch(...)
+    {
+        printf("Excption occurred");
+    }
+    
 }
--- mainAss.asm
+++ mainAss.asm
@@ -1,5 +1,41 @@
+nl      = 10
+maxLen  = 256
+
+.data 
+    titleStr    byte "NyaStuff", 0
+    prompt      byte "Enter something: ", 0 
+    fmtStr      byte "User entered: '%s'", nl, 0
+    input       byte maxLen dup (?)
+
 .code
+
+externdef printf:proc
+externdef readLine:proc
+
+public getTitle
+getTitle proc
+    lea rax, titleStr
+    ret
+getTitle endp
+
+public nya
 nya proc
+    sub rsp, 56
+
+    lea rcx, prompt
+    call printf
+    
+    mov input, 0
+    lea rcx, input
+    mov rdx, maxLen
+    call readLine
+
+    lea rcx, fmtStr
+    lea rdx, input
+    call printf
+
+    add rsp, 56
+
     ret
 nya endp
 end
