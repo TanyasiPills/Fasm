009e80fc7c19b9d7c04f890034128deddbda4a88
Maychii <106334255+TanyasiPills@users.noreply.github.com>
2025-03-20 08:46:19 +0100

    batch file upgrade

--- run.bat
+++ run.bat
@@ -1,3 +1,10 @@
 echo off
 ml64 /nologo /c /Zi /Cp %1.asm
 cl /nologo /O2 /Zi /utf-8 /EHa /Femain.exe main.cpp %1.obj
+del /Q "%~dp0main.ilk"
+del /Q "%~dp0main.obj"
+del /Q "%~dp0main.pdb"
+del /Q "%~dp0vc140.pdb"
+del /Q "%~dp0%1.obj"
+
+echo build completed
